e56175837cb7668a57ef7f49721ab0cfa5d0de55
christina <xristitina@gmail.com>
2020-01-28 20:13:23 +0200

    creates a the header component and the app layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Header.scss';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header className="text-center">
+                <h1 className="m-0">GitHub Client</h1>
+            </header>
+        );
+    }
+}
