699bd84cb6c49604f2658ce7c315174f9680c1e3
christina <xristitina@gmail.com>
2020-01-30 21:43:43 +0200

    modifies Following page template

--- src/pages/Following/Following.tsx
+++ src/pages/Following/Following.tsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 export default class Following extends Component {
     render() {
-        return <div>Following Page</div>;
+        return (
+            <div className="mt-5">
+                <div className="row mb-4">
+                    <div className="col">Following Page</div>
+                </div>
+            </div>
+        );
     }
 }
