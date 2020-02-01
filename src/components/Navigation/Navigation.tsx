93d4794c842783305fc6f6bae77a1bd9f29a3df8
christina <xristitina@gmail.com>
2020-02-01 11:28:19 +0200

    creates a NavigationState interface

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { NavigationStateInterface } from '../../interfaces/NavigationState.interface';
+
+export default class Navigation extends Component<{}, NavigationStateInterface> {
+    state: NavigationStateInterface = {
+        currentPath: 'github-users',
+    };
 
 export default class Navigation extends Component {
     render() {
