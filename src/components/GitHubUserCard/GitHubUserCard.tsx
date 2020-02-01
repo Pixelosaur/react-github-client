23c110d3e389a876cab5a23e56e8ebe4113b5b24
christina <xristitina@gmail.com>
2020-02-01 18:25:51 +0200

    updates userCard component layout and adds styles

--- src/components/GitHubUserCard/GitHubUserCard.tsx
+++ src/components/GitHubUserCard/GitHubUserCard.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './GitHubUserCard.scss';
-import { UserInterface } from '../../interfaces/User.interface';
+import { Link } from 'react-router-dom';
 import { UserDetailsInterface } from '../../interfaces/UserDetails.interface';
 
 type Props = {
@@ -10,7 +10,7 @@ type Props = {
 export default class GitHubUserCard extends Component<Props, {}> {
     render() {
         return (
-            <div className="col-sm-12 col-md-6 col-lg-3">
+            <div className="col-sm-12 col-md-6 col-lg-4">
                 <div className="card mb-4">
                     <img
                         src={this.props.user.avatar_url}
@@ -19,21 +19,46 @@ export default class GitHubUserCard extends Component<Props, {}> {
                     />
                     <div className="card-body">
                         <h5 className="card-title">{this.props.user.name}</h5>
-                        <h6 className="card-subtitle mb-3 text-muted">{this.props.user.login}</h6>
-                        <p className="card-text">
-                            Some quick example text to build on the card title and make up the bulk
-                            of the card's content.
-                        </p>
+                        <h6 className="card-subtitle mb-3">{this.props.user.login}</h6>
+                        <div className="location-wrapper pt-2">
+                            <i className="fas fa-map-marker-alt mr-2" />
+                            <span>
+                                {this.props.user.location ? this.props.user.location : 'N/A'}
+                            </span>
+                        </div>
 
-                        <div className="meta-wrapper">
-                            <ul>
-                                <li>location</li>
+                        <div className="mb-3 mt-3">
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">
+                                    <span>Public Repos</span>
+                                    <span className="badge badge-pill badge-primary">
+                                        {this.props.user.public_repos}
+                                    </span>
+                                </li>
+                                <li className="list-group-item">
+                                    <span>Public Gists</span>
+                                    <span className="badge badge-pill badge-primary">
+                                        {this.props.user.public_gists}
+                                    </span>
+                                </li>
+                                <li className="list-group-item">
+                                    <span>Followers</span>
+                                    <span className="badge badge-pill badge-primary">
+                                        {this.props.user.followers}
+                                    </span>
+                                </li>
+                                <li className="list-group-item">
+                                    <span>Following</span>
+                                    <span className="badge badge-pill badge-primary">
+                                        {this.props.user.following}
+                                    </span>
+                                </li>
                             </ul>
                         </div>
 
-                        <a href="#" className="card-link">
-                            View Details
-                        </a>
+                        <Link className="card-link" to="/">
+                            View Repositories
+                        </Link>
                     </div>
                 </div>
             </div>
