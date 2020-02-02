d5f13886ed7363ca0134eacd63e4549c8ff4b7ba
christina <xristitina@gmail.com>
2020-02-02 11:28:53 +0200

    extracts the username from url params and gets the list of user repos

--- src/pages/GitHubUser/GitHubUser.tsx
+++ src/pages/GitHubUser/GitHubUser.tsx
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
 import { GitHubUserPropsInterface } from '../../interfaces/GitHubUserProps.interface';
+import axios, { AxiosResponse } from 'axios';
+import { GitHubUserReposApiResponseInterface } from '../../interfaces/GitHubUserReposApiResponse.interface';
 
 export default class GitHubUser extends Component<GitHubUserPropsInterface, {}> {
+    componentDidMount(): void {
+        this.getUserRepos();
+    }
+
+    /* extract username from url params */
+    getUrlParams(): string {
+        const {
+            match: { params },
+        } = this.props;
+
+        return params.username;
+    }
+
+    getUserRepos(): void {
+        const url: string = `https://api.github.com/users/${this.getUrlParams()}/repos`;
+        const params = {
+            sort: 'updated',
+            type: 'owner',
+            direction: 'desc',
+            page: 1,
+            per_page: 10,
+        };
+
+        axios
+            .get<GitHubUserReposApiResponseInterface>(url, { params })
+            .then((response: AxiosResponse<GitHubUserReposApiResponseInterface>) => {
+                console.log(response.data);
+            })
+            .catch((error: any) => console.log(error));
+    }
+
     render() {
         return (
             <div className="mt-5 mb-5">
